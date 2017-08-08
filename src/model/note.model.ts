6429fa155686a1971678a8fe05109e9fe218ce0e
Synnex1 <mike_morthorst@hotmail.de>
2017-08-08 16:04:55 +0200

    addnewnote

--- src/model/note.model.ts
+++ src/model/note.model.ts
@@ -2,7 +2,7 @@ export class Note {
 	type: string = "";
 	title: string = "";
 	description: string = "";
-	location: {lat: number, lng: number};
+	location: {lat: number, lng: number} = null;
 	
 	constructor(argument) {
 		// code...
--- src/pages/addNotePage/addNotePage.ts
+++ src/pages/addNotePage/addNotePage.ts
@@ -4,13 +4,14 @@ import { MyLocationPage } from '../my-location/my-location';
 import { NoteService } from '../../services/note.service';
 import { NavController, ModalController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { Note } from '../../model/note.model';
 
 @Component({
   selector: 'addNotePage',
   templateUrl: 'addNotePage.html',
 })
 export class AddNotePage {
-  location: {lat: number, lng: number} = {lat: 51.1758388, lng:10.929209};
+  note: Note;
 
   constructor(private noteService: NoteService,
               private navCtrl: NavController,
@@ -18,15 +19,19 @@ export class AddNotePage {
               private modalCtrl: ModalController) {
   }
 
-  public addNewNote(value: {title: any, description: string }) {
-    this.noteService.addNote({title: value.title,description: value.description, location: this.location});
+  public addNewNote(value: {type: string, title: string, description: string}) {
+    this.note.type = value.type;
+    this.note.title = value.title;
+    this.note.description = value.description;
+
+    this.noteService.addNote(this.note);
     console.log('new note was added to the list');
     this.navCtrl.pop();    
   }
 
   public onOpenAddMyLocation(): void{
     //this.navCtrl.push(MyLocationPage);
-    this.modalCtrl.create(MyLocationPage, {location: this.location}).present();
+    this.modalCtrl.create(MyLocationPage, {location: this.note.location}).present();
     console.log('OPENADDMYLOCATION WAS CLICKED');
   }
 
