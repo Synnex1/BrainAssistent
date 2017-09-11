3245c645c27f320d334d2e8e1f2e10a193d06f61
Mike_Morthorst <mike_morthorst@hotmail.de>
2017-09-11 19:06:52 +0200

    reminder not working yet

--- src/pages/reminder/reminder.ts
+++ src/pages/reminder/reminder.ts
@@ -1,26 +1,22 @@
 import { Component } from '@angular/core';
 import { ViewController, AlertController, Platform } from 'ionic-angular';
 import { LocalNotifications } from '@ionic-native/local-notifications';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-reminder',
   templateUrl: 'reminder.html',
 })
 export class ReminderPage {
-  private dateDate: Date;
-  private myDate: string;
-  private minDate: string;
-  myNotification: string;
+  displayDate: any;
+  minDate: any;
+  notifyDate: any;
+  notifications: any[] = [];
 
   constructor(public viewCtrl: ViewController, public platform: Platform, public alertCtrl: AlertController, private localNotifications: LocalNotifications) {
     //Setting up the dateTime-Picker 
-    let now = new Date();
-    now.setHours(now.getHours() - (now.getTimezoneOffset() / 60));
-    this.dateDate = now;
-    this.myDate = now.toISOString();
-
-    this.minDate = this.myDate;
-
+    this.displayDate = moment(new Date()).format();
+    this.minDate = this.displayDate;
     /*
     this.platform.ready().then((rdy => {
       this.localNotifications.on('click', (notification, state) => {
@@ -41,24 +37,16 @@ export class ReminderPage {
       title: 'Attention',
       text: 'Notification',
       sound: 'file://assets/sounds/notification.mp3',
-      at: this.myDate
+      at: this.notifyDate
     });
-  }
-
-  showMyDate() {
-    console.log(this.dateDate);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReminderPage');
   }
 
-  
-  setReminder(myDate, myNotification){
-    //return data to addNotePage ------------------------------------------------->
-    //then
-    
-    this.viewCtrl.dismiss();
+  onChangeDate(date) {
+    this.notifyDate = new Date(date.year.value, date.month.value, date.day.value, date.hour.value, date.minute.value);
   }
   
   dismissReminder(){
