2cf890231b07dc933f208b18d88f918dcd73b5ed
Andi757 <andrejeichwald@gmx.de>
2017-09-13 18:13:06 +0200

    mal versuchen

--- src/pages/reminder/reminder.ts
+++ src/pages/reminder/reminder.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 import { ViewController, AlertController, Platform } from 'ionic-angular';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import { Note } from '../../model/note.model';
